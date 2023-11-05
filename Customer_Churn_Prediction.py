f71bb76cdbbd93bc6101a6f0ca9ef2ef60943dab
Vishal <Vishalarya1706@gmail.com>
2023-11-05 12:17:14 +0530

    Added some Graphics in the ipny

--- Customer_Churn_Prediction.py
+++ Customer_Churn_Prediction.py
@@ -12,17 +12,21 @@ def predict(input_data):
     return prediction
 
 # Streamlit UI elements for user input
-credit_score = st.slider('Credit Score', min_value=300, max_value=850, step=1, value=650)
-age = st.slider('Age', min_value=18, max_value=100, step=1, value=30)
+credit_score = st.number_input('Credit Score')
+age = st.number_input('Age', min_value=18, max_value=100 )
 tenure = st.slider('Tenure', min_value=0, max_value=10, step=1, value=5)
 balance = st.number_input('Balance', value=0.0)
 num_of_products = st.slider('Number of Products', min_value=1, max_value=4, step=1, value=1)
 has_cr_card = st.checkbox('Has Credit Card')
 is_active_member = st.checkbox('Is Active Member')
 estimated_salary = st.number_input('Estimated Salary', value=0.0)
-geography_germany = st.checkbox('Geography Germany')
-geography_spain = st.checkbox('Geography Spain')
-gender_male = st.checkbox('Gender Male')
+st.write("Select Your Geography")
+geography_germany = st.checkbox('Germany')
+geography_spain = st.checkbox('Spain')
+geography_spain = st.checkbox('France')
+st.write("Select Your Gender")
+gender_male = st.checkbox('Male')
+gender_female = st.checkbox('Female')
 
 # Combine user inputs into an array for prediction
 input_data = np.array([credit_score, age, tenure, balance, num_of_products, has_cr_card, is_active_member, estimated_salary,
